b23e5953e16c231c3f72eeb0c5bd4d176738a2c2
dinakaranak <dinakaranak001@gmail.com>
2025-09-30 12:51:49 +0530

    push

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-600'>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
